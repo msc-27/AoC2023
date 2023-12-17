40622efd6dd19fc9d4ecdd81cf397a8db0ca567a
Mark <mark@msc27.me.uk>
2023-12-17 12:02:21 +0000

    Day 7: more natural sorting key and simpler joker handling

--- python/day07.py
+++ python/day07.py
@@ -6,21 +6,16 @@ ssv = [line.split() for line in lines]
 rank = [ {y:x for x,y in enumerate('23456789TJQKA')}, \
          {y:x for x,y in enumerate('J23456789TQKA')} ]
 
-profile_to_type = { (5,): 6, (1,4): 5, (2,3): 4, (1,1,3): 3, \
-                    (1,2,2): 2, (1,1,1,2): 1, (1,1,1,1,1): 0 }
-
-def handtype(hand, part):
+def score(hand, part):
     ctr = Counter(hand)
     if part == 1 and 'J' in ctr and len(ctr) > 1:
         n_jokers = ctr.pop('J')
-        best_nonjoker = max(ctr, key=lambda x: ctr[x])
-        ctr[best_nonjoker] += n_jokers
-    return profile_to_type[tuple(sorted(ctr.values()))]
-
-def score(hand, part):
-    score = 0x100000 * handtype(hand, part)
-    score += sum((16**(4-i) * rank[part][hand[i]] for i in range(5)))
-    return score
+    else:
+        n_jokers = 0
+    profile = sorted(ctr.values(), reverse=True)
+    profile[0] += n_jokers
+    cards = [rank[part][c] for c in hand]
+    return (profile, cards)
 
 for part in [0,1]:
     ssv.sort(key = lambda x: score(x[0], part))
